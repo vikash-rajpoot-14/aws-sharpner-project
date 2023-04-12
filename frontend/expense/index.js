bb6ed070021430cec91abb788f36b9c9725c6f8b
vikash rajpoot <vikashraj1490@gmail.com>
2023-04-13 00:40:06 +0530

    prium user frontr=end tabledone

--- frontend/expense/index.js
+++ frontend/expense/index.js
@@ -3,9 +3,11 @@ const error = document.getElementById("error");
 const leaderboard = document.getElementById("leaderboard");
 const listboard = document.getElementById("listboard");
 const leaderbtn = document.getElementById("leader-btn");
+const download = document.getElementById("download");
 leaderboard.style.display = "none";
 listboard.style.display = "none";
 leaderbtn.style.display = "none";
+download.style.display = "none";
 let token = localStorage.getItem("token");
 let decode = parseJwt(token);
 showdata();
@@ -58,34 +60,31 @@ async function showHandler(e) {
 async function showdata() {
   let token = localStorage.getItem("token");
   const decode = parseJwt(token);
-  // const token2 = localStorage.getItem("token2");
-  // const decode2 = parseJwt(token2);
+
   const expenses = await axios.get("http://localhost:3000/expenses", {
     headers: {
       "Content-type": "application/json",
       authorization: "Bearer " + `${token}`,
     },
   });
+
   if (decode.ispremiumuser) {
     buttonChange();
   }
-  // if (decode2.ispremiumuser) {
-  //   buttonChange();
-  // }
   let listData = "";
   if (expenses.data.data.length < 1) {
     ul.innerHTML = listData;
   } else {
     expenses.data.data.map((expense) => {
-      listData += '<li class="list-item">';
-      listData += `${expense.expense} ${expense.description} ${expense.category}  `;
+      listData += '<li class="list">';
+      listData += ` ${expense.expense} ${expense.description} ${expense.category}   `;
       listData +=
         "<button class='btn-delete' onclick='deleteData(`" +
         expense.id +
-        "`)'>delete</button>";
+        "`)'>delete</button> ";
       listData += "</li>";
-      ul.innerHTML = listData;
     });
+    ul.innerHTML = listData;
   }
 }
 
@@ -170,7 +169,8 @@ document.getElementById("rzp-button1").onclick = async function (e) {
 leaderbtn.onclick = function leader() {
   leaderboard.style.display = "block";
   listboard.style.display = "block";
-  leaderboardData();
+  download.style.display = "block";
+  leaderboardTableData();
 };
 
 async function buttonChange() {
@@ -178,7 +178,7 @@ async function buttonChange() {
   premiumButton.innerHTML = "you are a premium user now";
   premiumButton.setAttribute("disabled", "");
   leaderbtn.style.display = "block";
-  leaderboardData();
+  leaderboardTableData();
 }
 
 async function leaderboardData() {
@@ -210,5 +210,85 @@ async function leaderboardData() {
       listData += "</li>";
       listboard.innerHTML = listData;
     });
+  }
+}
+
+async function leaderboardTableData() {
+  const response = await axios.get("http://localhost:3000/expenses", {
+    headers: {
+      "Content-type": "application/json",
+      authorization: "Bearer " + `${token}`,
+    },
+  });
+  // console.log(response.data.data);
+  let expenses = response.data.data;
+  let arr = [...expenses];
+  // for (let i = 0; i < arr.length; i++) {
+  //   arr[i].expenses = arr[i].expenses.reduce((a, b) => a + b.expense * 1, 0);
+  // }
+  // arr.sort((a, b) => b.expenses - a.expenses);
+  const d = Date.now();
+  const now = new Date(d);
+  const monthName = now.toLocaleString("default", { month: "long" }); // Get the month name using toLocaleString()
+  const year = now.getFullYear();
+  const monthnumber = now.getMonth();
+  let cmonth = String(monthnumber + 1);
+  cmonth = "0" + cmonth;
+  // const month = arr[0].createdAt.slice(0, 10).split("-")[1];
+  arr = arr.filter(
+    (expense) => expense.createdAt.slice(0, 10).split("-")[1] === cmonth
+  );
+  let totalIncome = 0;
+  let totalExpense = 0;
+  let totalSaving = 0;
+  let listData = "";
+  listData += `<h2>${monthName} ${year}</h2>`;
+  listData += "<table>";
+  listData +=
+    "<tr><th>Date</th><th>Description</th><th>Category</th><th>Income</th><th>Expenses</th></tr>";
+  if (arr.length < 1) {
+    listData += "</table>";
+    listboard.innerHTML = listData;
+  } else {
+    arr.map((expense) => {
+      listData += '<tr class="table-item">';
+      if (expense.category === "salary") {
+        totalIncome += expense.expense * 1;
+        listData += `<th>${expense.createdAt.slice(0, 10)}</th><th>${
+          expense.description
+        }</th><th>${expense.category}</th><th>${
+          expense.expense
+        }.00</th><th>00.00</th> `;
+      } else {
+        totalExpense += expense.expense * 1;
+        listData += `<th>${expense.createdAt.slice(0, 10)}</th><th>${
+          expense.description
+        }</th><th>${expense.category}</th><th>00.00</th><th>${
+          expense.expense
+        }.00</th> `;
+      }
+      listData += "</tr>";
+    });
+    totalSaving = totalIncome - totalExpense;
+    listData += `<tr><th></th><th></th><th></th><th>${totalIncome}.00</th><th>${totalExpense}.00</th></tr>`;
+    listData += `<tr><th></th><th></th><th></th><th style= "color : green ; width:80px">$ ${totalIncome}.00</th><th style= "color : red ; width:60px">$ ${totalExpense}</th></tr>`;
+    listData += `<table style = "width: 80%;"><tr><th style= "color : blue; text-align: right; ">Total Saving :- $ ${totalSaving}.00</th></tr></table>`;
+    listData += "</table>";
+    // mothly table
+    listData += "<h3>Yearly Report</h3>";
+    listData += "<table>";
+    listData +=
+      "<tr><th>Month</th><th>Income</th><th>Expense</th><th>Saving</th></tr>";
+    listData += `<tr><th>${monthName}</th><th> ${totalIncome}.00</th><th>${totalExpense}.00</th><th> ${totalSaving}.00</th></tr>`;
+    listData += `<tr><th></th><th style= "color : green">$ ${totalIncome}.00</th><th style= "color : red">$ ${totalExpense}.00</th><th style= "color : blue">$ ${totalSaving}.00</th></tr>`;
+    listData += "</table>";
+    // notes table
+    listData += `<h3>Notes Report ${year}</h3>`;
+    listData += "<table>";
+    listData += "<tr><th>Date</th><th>Notes</th></tr>";
+    listData += "<tr><th>02-04-2023</th><th>have to go to doctor</th></tr>";
+    listData += "<tr><th>06-04-2023</th><th>meet at sharpner </th></tr>";
+    listData += "</table>";
+    listboard.innerHTML = listData;
   }
 }
